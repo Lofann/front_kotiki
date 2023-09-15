366813cb0091ebb25c2fe040c96f6bda608f4e06
Арина Андреевна <Arina.Sheshukova@urfu.me>
2023-09-15 05:03:51 +0500

    Add taskmaker's form

--- .nuxt/layouts/default.vue
+++ .nuxt/layouts/default.vue
@@ -1,3 +1,22 @@
 <template>
-  <Nuxt />
+
+
+  <div>
+    <NavbarRole/>
+    <main>
+      <div class = 'container'>
+    <Nuxt />
+  </div>
+    </main>
+  </div>
+
 </template>
+
+<script>
+import NavbarRole from "/components/NavbarRole"
+export default{
+  components:{
+    NavbarRole
+  }
+}
+</script>
